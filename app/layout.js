1030d0cdc24198af4a92779649348af21f698a7c
Ana Marisa <ana.marisa.mf@gmail.com>
2025-06-02 15:43:28 +0700

    add favicon

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,11 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "Imperium",
+  icons: {
+    icon: "/favicon.svg",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
